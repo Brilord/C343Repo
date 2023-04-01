6a2a3c826aa3d4d7d27fb587992650cd8d2b05a8
Brian W <95247252+Brilord@users.noreply.github.com>
2023-04-01 17:19:53 -0400

    working progress

---
+++ C343Assignments/Lab9/src/Lab9.java
@@ -0,0 +1,84 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lab9 {
+
+    /** RADIX SORT
+     *
+     * This is NOT a recursive algorithm in the normal sense
+     * This is to iterate through the digits of each number
+     *
+     * This method simply iterates through the number of digits in the max
+     * element and calls radixHelper
+     *
+     * @param arr array to be sorted
+     */
+    static void radixSort(int[] arr) {
+        //TODO
+    }
+
+    /** RADIX HELPER
+     *
+     * Words do not do it justice. For a visualization see:
+     * https://www.cs.usfca.edu/~galles/visualization/RadixSort.html
+     *
+     * @param arr array to be sorted
+     * @param place the placement of the digit you are on
+     */
+    static void radixHelper(int[] arr, int place) {
+        //TODO
+    }
+
+    /** BUCKET SORT
+     *
+     * you are allowed to use Collections.Sort ONLY for sorting the individual buckets
+     *
+     * bucket sort is a scatter and gather sorting algorithm where you split all the
+     * elements into "buckets" that correspond with which range the number lies in.
+     *
+     * You must first get the maximum and minimum elements and then calculate the ranges
+     * the buckets will use.
+     *
+     * hint: (max - min) / number of buckets
+     *
+     * You must then disperse the elements into their corresponding buckets
+     *
+     * Then sort the buckets. (This is where you may use Collections.Sort or copy code
+     * from the previous lab idc)
+     *
+     * Lastly "dump" the buckets back into the arr and you are done!
+     *
+     * @param arr array to be sorted
+     * @param noOfBuckets the number of buckets to be used
+     */
+    static void bucketSort(int[] arr, int noOfBuckets)
+    {
+        //TODO
+    }
+
+    /** MAX
+     * useful for bucket sort and radix sort
+     *
+     * iterate through the array and return the largest element
+     *
+     * @param arr array of integers
+     * @return the maximum element of the array
+     */
+    public static int max(int[] arr) {
+        //TODO
+    }
+
+    /** MIN
+     * useful for bucketsort
+     *
+     * iterate through the array and return the smallest element
+     *
+     * @param arr array of integers
+     * @return the minimum element of the array
+     */
+    public static int min(int[] arr) {
+        //TODO
+    }
+
+}
---
+++ C343Assignments/Lab9/test/Lab9Test.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+class Lab9Test {
+
+    @Test
+    void bucketSort() {
+        //TODO
+    }
+
+    @Test
+    void radixSort() {
+        //TODO
+    }
+
+    @Test
+    void max() {
+        //TODO
+    }
+
+    @Test
+    void min() {
+        //TODO
+    }
+}
