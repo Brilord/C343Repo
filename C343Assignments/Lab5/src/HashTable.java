634b651fc6c22e2b6967fabd4f2a48d9eab2204f
Brilord <95247252+Brilord@users.noreply.github.com>
2023-05-05 02:04:05 -0400

    working progress

--- C343Assignments/Lab5/src/HashTable.java
+++ C343Assignments/Lab5/src/HashTable.java
@@ -9,7 +9,10 @@ public class HashTable {
     private List<LinkedList<Entry>> entries;
 
     public HashTable() {
-
+        entries = new ArrayList<>(capacity);
+        for(int i = 0; i < capacity; i++) {
+            entries.add(new LinkedList<>());
+        }
     }
 
     public List<LinkedList<Entry>> getEntries() {
@@ -21,16 +24,37 @@ public class HashTable {
     }
 
     private int hash(String key) {
-
+        // simple hash function to get an index for the key
+        int hash = key.hashCode() % capacity;
+        // insure the index is positive
+        return hash < 0 ? hash + capacity : hash;
     }
 
 
     public void put(String key, String value) {
-
+        int index = hash(key);
+        LinkedList<Entry> bucket = entries.get(index);
+        for(Entry entry : bucket) {
+            if(entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+        }
+        bucket.add(new Entry(key, value));
+        size++;
+        if((double)size / capacity >= 0.75) {
+            rehash();
+        }
     }
 
     public String get(String key) {
-
+        int index = hash(key);
+        LinkedList<Entry> bucket = entries.get(index);
+        for(Entry entry : bucket) {
+            if(entry.key.equals(key)) {
+                return entry.value;
+            }
+        }
     }
 
     public void remove(String key) {
