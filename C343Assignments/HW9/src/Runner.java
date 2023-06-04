90d2877a5c41c261586a0390e2735254f1606c42
Brian W <brilord1000@gmail.com>
2023-06-04 10:06:53 -0400

    Committing...

--- C343Assignments/HW9/src/Runner.java
+++ C343Assignments/HW9/src/Runner.java
@@ -14,28 +14,50 @@ import java.io.*;
  */
 public class Runner {
 
-    public static void main(String[] args) throws IOException{
-        // TODO: write the runner so that it follows the same format as the directions
-        // read input file name from command line argument
-        if(args.length < 1) {
+    public static void main(String[] args) throws IOException {
+        // Read input file name from command line argument
+        if (args.length < 1) {
             System.out.println("Usage: java Runner <input_file>");
             System.exit(1);
         }
-
-        // read input file
+    
+        // Read input file
         String inputFile = args[0];
         File file = new File(inputFile);
         Scanner scanner = new Scanner(file);
-        // read the picture from the input file
+    
+        // Read the picture from the input file
         Picture picture = new Picture(scanner.next());
-
-        // create a SeamCarver object
-        SeamCarver seeamCarver = new SeamCarver(picture);
-        
-        // read and perform the operation.
-        while(scanner.hasNext()) {
+    
+        // Create a SeamCarver object
+        SeamCarver seamCarver = new SeamCarver(picture);
+    
+        // Read and perform operations
+        while (scanner.hasNext()) {
             String operation = scanner.next();
-            
+            if (operation.equals("findVerticalSeam")) {
+                int[] vSeam = seamCarver.findVerticalSeam();
+                System.out.println(Arrays.toString(vSeam));
+            } else if (operation.equals("findHorizontalSeam")) {
+                int[] hSeam = seamCarver.findHorizontalSeam();
+                System.out.println(Arrays.toString(hSeam));
+            } else if (operation.equals("removeVerticalSeam")) {
+                int[] seam = new int[picture.height()];
+                for (int i = 0; i < seam.length; i++) {
+                    seam[i] = scanner.nextInt();
+                }
+                seamCarver.removeVerticalSeam(seam);
+            } else if (operation.equals("removeHorizontalSeam")) {
+                int[] seam = new int[picture.width()];
+                for (int i = 0; i < seam.length; i++) {
+                    seam[i] = scanner.nextInt();
+                }
+                seamCarver.removeHorizontalSeam(seam);
+            }
         }
+    
+        // Close the scanner
+        scanner.close();
     }
+    
 }
