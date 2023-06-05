887b8875d5ca21ad183fb8959b375ead70ed1c96
Brilord <brilord1000@gmail.com>
2023-06-05 11:21:29 -0400

    Committing...

--- C343Assignments/HW3/src/Queue.java
+++ C343Assignments/HW3/src/Queue.java
@@ -7,21 +7,28 @@ public class Queue<E> {
     // TODO: default constructor
     public Queue(){
         q = new DoublyLinkedList<>();
+        size = 0;
     }
 
     // TODO: Put element at end of queue
+    // this will add the value to the end of th queue
     public void enqueue(E elem){
-
+        q.insertAtTail(elem);
+        size++;
     }
 
     // TODO: Return the head of the queue; If there's nothing to return then throw EmptyQueueE
-    public E dequeue() throws EmptyQueueE {
-        if(q.isEmpty())
+    public E dequeue() throws EmptyQueueE, EmptyListE {
+        if(q.equals(null)) {
+            throw new EmptyQueueE();
+        }
+        size--;
+        return q.deleteAtHead();
     }
 
     // TODO: Without affecting the queue, return the element at the top of the queue
-    public E peek() throws IndexOutOfBoundsException{
-        return null;
+    public E peek() throws IndexOutOfBoundsException,{
+        
     }
 
     public int size() {
@@ -30,6 +37,10 @@ public class Queue<E> {
 
     // TODO: Checks if inputted is the same Queue
     public boolean equals(Object o){
+        if(o == this) {
+            return true;
+        }
+        if(!o inst)
         return false;
     }
 
